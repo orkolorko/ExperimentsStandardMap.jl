907c8ed9ce52a3e2bbc5814954d5234fb79ca7fe
Documenter.jl <documenter@juliadocs.github.io>
2022-08-05 06:50:47 +0000

    build based on d70e892

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ExperimentsStandardMap","category":"page"},{"location":"#ExperimentsStandardMap","page":"Home","title":"ExperimentsStandardMap","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ExperimentsStandardMap.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ExperimentsStandardMap]","category":"page"}]
+}
