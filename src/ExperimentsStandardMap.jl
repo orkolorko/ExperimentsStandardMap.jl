023c386bbb0d03b9b5e29fd2781360f1bd1bd816
Isaia Nisoli <nisoli@im.ufrj.br>
2022-08-05 15:49:50 +0900

    Simple implementation of the standard map

--- src/ExperimentsStandardMap.jl
+++ src/ExperimentsStandardMap.jl
@@ -1,5 +1,20 @@
 module ExperimentsStandardMap
 
-# Write your package code here.
+export F, orbit
+
+F(u::Vector; L = 1.0) = [mod(u[1]+(L/2π)*sin(2π*u[2]), 1); mod(u[2]+u[1]+(L/2π)*sin(2π*u[2]), 1)] 
+
+F(u::Matrix; L = 1.0) = vcat(mod.(u[1,:]+(L/2π)*sin.(2π*u[2,:]), 1)', mod.(u[2, :]+u[1, :]+(L/2π)*sin.(2π*u[2, :]), 1)')
+
+function orbit(v::Matrix{T}, n, L) where {T}
+    i, j = size(v)
+    out_orbit = zeros(T, (2*n, j))
+    out_orbit[1:2, :] = v
+    for i in 2:n
+        v = F(v; L = L)
+        out_orbit[2*i-1:2*i, :] = v
+    end
+    return out_orbit
+end
 
 end
